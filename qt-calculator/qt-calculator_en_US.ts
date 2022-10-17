d874ece5a4ee47724c4a237dc342720836ec3ec6
Max <you@example.com>
2022-10-16 23:52:44 -0700

    segmented font test

--- qt-calculator/qt-calculator_en_US.ts
+++ qt-calculator/qt-calculator_en_US.ts
@@ -1,3 +1,97 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="en_US"></TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>calculator</name>
+    <message>
+        <location filename="calculator.ui" line="14"/>
+        <source>calculator</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="calculator.ui" line="433"/>
+        <source>9</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="calculator.ui" line="69"/>
+        <source>/</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="calculator.ui" line="321"/>
+        <source>4</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="calculator.ui" line="461"/>
+        <source>1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="calculator.ui" line="181"/>
+        <source>8</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="calculator.ui" line="377"/>
+        <source>7</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="calculator.ui" line="405"/>
+        <source>AC</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="calculator.ui" line="349"/>
+        <source>5</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="calculator.ui" line="97"/>
+        <source>3</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="calculator.ui" line="125"/>
+        <source>6</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="calculator.ui" line="489"/>
+        <source>2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="calculator.ui" line="153"/>
+        <source>0</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="calculator.ui" line="237"/>
+        <source>=</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="calculator.ui" line="265"/>
+        <source>*</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="calculator.ui" line="293"/>
+        <source>+</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="calculator.ui" line="209"/>
+        <source>-</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="calculator.ui" line="514"/>
+        <source>0.0</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
